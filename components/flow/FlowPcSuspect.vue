5935ff91e26f06f35461fa9a0a7737963c9f6e6e
bpr-gunma <62873162+bpr-gunma@users.noreply.github.com>
2020-04-21 19:20:31 +0900

    add contents

--- components/flow/FlowPcSuspect.vue
+++ components/flow/FlowPcSuspect.vue
@@ -52,7 +52,7 @@
     <div :class="[$style.SubtleBox, $style.Box2, $style.Center]">
       <div :class="$style.ContentText, $style.Left">
         {{ $t('かかりつけ医にご相談ください。') }}<br />
-        {{ $t('かかりつけ医がいない方は、「新型コロナ感染症コールセンター」にご相談ください。') }}<br />
+        {{ $t('かかりつけ医がいない方は、「新型コロナ感染症コールセンター」にご相談ください。') }}
       </div>
       <div :class="$style.MediumText">
         {{ $t('新型コロナ感染症コールセンター') }}
@@ -197,7 +197,7 @@
 }
 
 .RowItemsHeader {
-//  font-size: medium;
+  font-size: medium;
 
   &Icon {
     display: block;
