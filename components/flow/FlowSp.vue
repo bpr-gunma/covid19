fb18a9721e921bd52f1edb867d6b8949da6030b6
bpr-gunma <62873162+bpr-gunma@users.noreply.github.com>
2020-04-25 10:29:44 +0900

    modify sp layout

--- components/flow/FlowSp.vue
+++ components/flow/FlowSp.vue
@@ -4,25 +4,16 @@
       {{ $t('新型コロナウイルス感染症にかかる相談窓口について') }}
     </h3>
     <div :class="$style.FlowCard">
+      <flow-sp-suspect />
+    </div>
+    <div :class="$style.FlowCard">
       <flow-sp-general />
     </div>
     <div :class="$style.FlowCard">
       <flow-sp-elder />
     </div>
-    <div :class="$style.FlowCard">
-      <flow-sp-past />
-    </div>
-    <div :class="$style.FlowCard">
-      <flow-sp-suspect />
-    </div>
     <div :class="[$style.FlowCard, $style.FlowCardGrayBg]">
       <flow-sp-advisory />
-    </div>
-    <div :class="$style.FlowCard">
-      <flow-sp-according />
-    </div>
-    <div :class="$style.FlowCard">
-      <flow-sp-hospitalized />
     </div>
   </div>
 </template>
@@ -30,23 +21,17 @@
 <script>
 import VueScrollTo from 'vue-scrollto'
 
-import FlowSpPast from './FlowSpPast.vue'
 import FlowSpGeneral from './FlowSpGeneral.vue'
 import FlowSpElder from './FlowSpElder.vue'
 import FlowSpSuspect from './FlowSpSuspect.vue'
 import FlowSpAdvisory from './FlowSpAdvisory.vue'
-import FlowSpAccording from './FlowSpAccording.vue'
-import FlowSpHospitalized from './FlowSpHospitalized.vue'
 
 export default {
   components: {
-    FlowSpPast,
     FlowSpGeneral,
     FlowSpElder,
     FlowSpSuspect,
     FlowSpAdvisory,
-    FlowSpAccording,
-    FlowSpHospitalized
   },
   mounted() {
     // ハッシュつきのURLにアクセスされたらすぐに遷移する
