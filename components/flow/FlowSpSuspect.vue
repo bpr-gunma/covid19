c73c3c935cfd74d14cffc4f37d5679b708bee754
bpr-gunma <62873162+bpr-gunma@users.noreply.github.com>
2020-04-25 23:21:14 +0900

    modify layout

--- components/flow/FlowSpSuspect.vue
+++ components/flow/FlowSpSuspect.vue
@@ -42,7 +42,7 @@
       href="#consult"
       :class="[$style.button, $style.clickable]"
     >
-      <span :class="$style.text">{{ $t('かかりつけ医がいない方は、新型コロナ感染症コールセンターへ') }}</span>
+      <span :class="$style.text">{{ $t('新型コロナ感染症コールセンターへ') }}</span>
       <ArrowForwardIcon :class="$style.icon" />
     </a>
   </div>
